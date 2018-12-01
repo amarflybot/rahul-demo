56c4ad58bb4a5deda52a77e8885f6f248031f7a0
amar.arawat@gmail.com <amar.arawat@gmail.com>
2018-12-01 18:02:24 +0530

    Change in pipeline

--- src/test/java/com/example/demorahul/AddServiceUnitTest.java
+++ src/test/java/com/example/demorahul/AddServiceUnitTest.java
@@ -1,7 +1,25 @@
 package com.example.demorahul;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class AddServiceUnitTest {
 
+    private AddService addService;
+
+    @Before
+    public void setUp() throws Exception {
+        addService = new AddService();
+    }
+
+    @Test
+    public void name() {
+        Result add = addService.add(1, 2);
+        Result expected = new Result();
+        expected.setA(1);
+        expected.setB(2);
+        expected.setResult(3);
+        Assert.assertEquals(expected,add);
+    }
 }
